766e8ccaa672490725aa23dcdea50b8cbcc21985
Er_cherry <67510331+Charvit123@users.noreply.github.com>
2021-04-01 12:23:49 +0530

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!-- Created By CodingNepal - www.codingnepalweb.com -->
 <html lang="en" dir="ltr">
   <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
